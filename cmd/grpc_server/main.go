d08a30010f69a95ba4fae7e1005146c5b1ddc3fe
xeeetu <vlad.alexeevstud@gmail.com>
2024-08-12 14:25:54 +0300

    сервер grpc

---
+++ cmd/grpc_server/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"log"
+	"net"
+
+	desc "github.com/xeeetu/gRPC/pkg/note_v1"
+)
+
+const grpcPort = ":50051"
+
+type server struct {
+	desc.UnimplementedNoteV1Server
+}
+
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("Note id: %d", req.GetId())
+
+	return &desc.GetResponse{
+		Note: &desc.Note{
+			Id: req.GetId(),
+			Info: &desc.NoteInfo{
+				Title:    gofakeit.BeerName(),
+				Content:  gofakeit.IPv4Address(),
+				Author:   gofakeit.Name(),
+				IsPublic: gofakeit.Bool(),
+			},
+			CreatedAt: timestamppb.New(gofakeit.Date()),
+			UpdatedAt: timestamppb.New(gofakeit.Date()),
+		},
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", grpcPort)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterNoteV1Server(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
